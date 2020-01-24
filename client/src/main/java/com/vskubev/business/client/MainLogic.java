571db99f984e6f71a9922327e0419c0603ebde51
vladimir <vvskubiev@gmail.com>
2020-01-24 20:09:03 +0300

    unused code removed

--- client/src/main/java/com/vskubev/business/client/MainLogic.java
+++ client/src/main/java/com/vskubev/business/client/MainLogic.java
@@ -29,34 +29,7 @@ public class MainLogic {
         while (true) {
             if (!isAuth) {
                 authorization.auth();
-
-//                System.out.println("1.sign up");
-//                System.out.println("2.sign in");
-//                BufferedReader reader = new BufferedReader(
-//                        new InputStreamReader(System.in));
-//                String answer = reader.readLine();
-//
-//                if ("1".equals(answer)) {
-//
-//                } else if ("2".equals(answer)) {
-//                    System.out.println("Enter login:");
-//                    String login = reader.readLine();
-//                    System.out.println("Enter password:");
-//                    String password = reader.readLine();
-//
-//                    ResourceOwnerPasswordResourceDetails clientDetails = new ResourceOwnerPasswordResourceDetails();
-//                    clientDetails.setAccessTokenUri("http://localhost:9090/oauth/token");
-//                    clientDetails.setClientId("example");
-//                    clientDetails.setClientSecret("examplesecret");
-//                    clientDetails.setGrantType("password");
-//                    clientDetails.setUsername(login);
-//                    clientDetails.setPassword(password);
-//
-//                    OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(clientDetails);
-//                    OAuth2AccessToken token = oAuth2RestTemplate.getAccessToken();
-//
-//                    int k = 10;
-                }
+            }
         }
     }
 }
